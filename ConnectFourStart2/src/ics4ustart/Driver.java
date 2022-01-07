3c54d2a63cdd3466525370d2f90d5d0cae518267
Grant Hutchison <grant.hutchison@tdsb.on.ca>
2022-01-07 13:04:46 -0500

    first drop

---
+++ ConnectFourStart2/src/ics4ustart/Driver.java
@@ -0,0 +1,70 @@
+package ics4ustart;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Text (console) based driver for testing purposes.
+ * @author Hutchison
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+
+		// Setup constants for the Board
+		final int ROWS = 7;
+		final int COLS = 7;
+
+		// create the board
+		Board board = new Board(ROWS, COLS);
+		board.display();
+		
+		// console input
+		Scanner in = new Scanner(System.in);
+		boolean done = false;
+		String value = "";
+		int column = 0;
+		CellState player = CellState.P1;
+			
+		while (!done) {
+			column = getColumn(in, 1, COLS); // include min and max
+			board.placePiece(column, player);
+			
+			// Check for winners
+			
+			board.display();
+			if (player == CellState.P1) {
+				player = CellState.P2;
+			}
+		}
+	}
+	
+/**
+ * Helper method to ensure column value is valid.
+ * @param in
+ * @return valid column number
+ */
+	private static int getColumn(Scanner in, int min, int max) {
+		boolean valid = false;
+		int column = 0;
+		
+		while (!valid) {
+			String prompt = String.format("Which column (%d,%d) :",min,max); 
+			System.out.print(prompt);
+			if (in.hasNextInt()) {
+				column = in.nextInt();
+				if (column >= min && column <= max) {
+					valid = true;
+				} else {
+					System.out.println("Invalid numeric value provided.");
+				}
+			} else {
+				System.out.println("Not a number.");
+			}
+			in.nextLine();
+		}
+		return column;
+	}
+}
