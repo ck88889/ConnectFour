bbdf49eedf15656d7efc62737694865b30dee83b
anastasiavassos <97248510+anastasiavassos@users.noreply.github.com>
2022-01-19 14:31:55 -0500

    Update Driver.java

--- ConnectFourStart2/src/ics4ustart/Driver.java
+++ ConnectFourStart2/src/ics4ustart/Driver.java
@@ -26,13 +26,16 @@ public class Driver {
 		System.out.println("Enter '1' for Single Player mode or '2' for Two Player mode: ");
 		int mode = in.nextInt();
 		
+		//initialize variables
 		boolean done = false;
 		String value = "";
 		int column = 0;
 		int turn = 0;
-			
+		
+		//loop that continues while the game is in progress
 		while (!done) {
 			turn = turn + 1;
+			//gets column number from corresponding player and places piece accordingly
 			if (checkTurn(turn) == 2) {
 				column = getColumn(in, 1, COLS, turn, mode); 
 				board.placePiece(column, CellState.P2);
@@ -43,7 +46,6 @@ public class Driver {
 			}
 			
 			// Check for winners
-
 			board.display();
 			if (board.checkAcross() == CellState.P1 || board.checkVertical() == CellState.P1|| board.checkDiagonalOne() == CellState.P1|| board.checkDiagonalTwo() == CellState.P1) {
 				done = true;
@@ -55,16 +57,18 @@ public class Driver {
 		}
 	}
 	
-/**
- * Helper method to ensure column value is valid.
- * @param in
- * @return valid column number
- */
+	/**
+ 	* Helper method to ensure column value is valid.
+ 	* @param in, min, max, turn, mode
+ 	* @return valid column number
+ 	*/
 	private static int getColumn(Scanner in, int min, int max, int turn, int mode) {
 		boolean valid = false;
 		int column = 0;
 		
-		if (mode == 2 || checkTurn(turn) ==1 ) {
+		//Prompts user for column number if two player mode has been selected or player one in single player mode
+		//Checks if column number is valid
+		if (mode == 2 || checkTurn(turn) == 1 ) {
 		while (!valid) {
 			String prompt = String.format("Player " + checkTurn(turn)+ ": Which column (%d,%d) :",min,max); 
 			System.out.print(prompt);
@@ -81,11 +85,17 @@ public class Driver {
 			in.nextLine();
 		}
 		}
+		//Generates a random column for the AI player in single player mode
 		else if(mode == 1) {
 			column = (int)(Math.random()* (max - min + 1)+ min);
 		}
 		return column;
 	}
+	/**
+ 	* Checks which player's turn it is
+ 	* @param turn - counter representing whose turn it is
+ 	* @return integer representing which player's turn it is
+ 	*/
 	public static int checkTurn(int turn) {
 		if (turn%2 == 0) {
 			return 2;
